ff33c6674241389d2f50fe19de0d28f7d2baa6c7
Amar Aiman Ab Jamal <amarabjamal@gmail.com>
2023-05-21 22:40:29 +0800

    Updated layout

--- resources/js/Pages/Lawyer/Shared/Sidebar.vue
+++ resources/js/Pages/Lawyer/Shared/Sidebar.vue
@@ -2,49 +2,19 @@
     <nav id="sidebar" :class="`${!isSidebarOpen ? 'close' : ''}`">
         <div class="menu-items">
             <ul class="nav-links">
-                <li>
+                <li v-for="(navigation, index) in navigations" :key="index">
                     <div>
-                        <Link href="/lawyer">
-                            <icon-sidebar name="home-smile-line" class="mr-2 w-5 h-5"></icon-sidebar>
-                            <span class="link-name">Dashboard</span>
+                        <Link :href="navigation.href">
+                            <icon-sidebar :name="navigation.icon"></icon-sidebar>
+                            <span class="link-name">{{ navigation.label }}</span>
                         </Link>
                     </div>
-                </li>
-                <li>
-                    <Link href="/lawyer/clients" :class="{'color-blue-500' : $page.url.startsWith('/lawyer/clients')}">
-                        <icon-sidebar name="group-line" class="mr-2 w-5 h-5"></icon-sidebar>
-                        <span class="link-name">Client Profiles</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/lawyer/case-files">
-                        <icon-sidebar name="folder-5-line" class="mr-2 w-5 h-5"></icon-sidebar>
-                        <span class="link-name">Case Files</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/lawyer">
-                        <icon-sidebar name="increase-decrease-line" class="mr-2 w-5 h-5"></icon-sidebar>
-                        <span class="link-name">Firm Accounts</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/lawyer">
-                        <icon-sidebar name="booklet-line" class="mr-2 w-5 h-5"></icon-sidebar>
-                        <span class="link-name">Client Accounts</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/lawyer">
-                        <icon-sidebar name="ticket-line"></icon-sidebar>
-                        <span class="link-name">Claim Vouchers</span>
-                    </Link>
                 </li>
             </ul>
             
             <ul v-if="$page.props.auth.user.roles.indexOf('admin') !== -1" class="bottom-container">
                 <li>
-                    <Link href="/admin" class="bg-blue-400 shadow">
+                    <Link href="/admin">
                         <icon-sidebar name="user-shared-fill" class="w-7 h-7"></icon-sidebar>
                         <span class="link-name color-slate-700  uppercase mr-2">Switch to Admin</span>
                     </Link>
@@ -60,6 +30,18 @@ import IconSidebar from '../../../Shared/IconSidebar';
 export default {
     props: {
         isSidebarOpen: Boolean,
+    },
+    data() {
+        return {
+            navigations: [
+                {href: '/lawyer', label: 'Dashboard', icon:'home-smile-line', isActive: this.$page.url.startsWith('/lawyer')},
+                {href: '/lawyer/clients', label: 'Client Profiles', icon:'group-line', isActive: this.$page.url.startsWith('/lawyer/clients')},
+                {href: '/lawyer/case-files', label: 'Case Files', icon:'folder-5-line', isActive: this.$page.url.startsWith('/lawyer/case-files')},
+                {href: '/lawyer', label: 'Firm Accounts', icon:'increase-decrease-line', isActive: this.$page.url.startsWith('/lawyer/firm-accounts')},
+                {href: '/lawyer', label: 'Client Accounts', icon:'booklet-line', isActive: this.$page.url.startsWith('/lawyer/client-accounts')},
+                {href: '/lawyer', label: 'Claim Vouchers', icon:'ticket-line', isActive: this.$page.url.startsWith('/lawyer/clients/claim-vouchers')},
+            ],
+        }
     },
     components: {
         IconSidebar,
