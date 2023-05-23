01c583c2edad117ae41fc7a4f9a53652f77afe1a
Amar Aiman Ab Jamal <amarabjamal@gmail.com>
2023-05-23 11:08:10 +0800

    Added modal component

---
+++ resources/js/Pages/Lawyer/Invoice/SelectItemModal.vue
@@ -0,0 +1,55 @@
+<template>
+    <TransitionRoot
+        :show="isOpen"
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+    >
+        <Dialog as="div" :open="isOpen" @close="closeModal" class="relative z-50">
+            <div class="fixed inset-0 bg-black/30" aria-hidden="true" />
+            <div class="fixed inset-0 flex items-center justify-center p-4">
+                <div class="fixed inset-0 z-10 overflow-y-auto">
+                    <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                        <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                            <div class="p-6 mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">Deactivate account</DialogTitle>
+                    
+                                <div class="mt-2">
+                                    <p class="text-sm text-gray-500">Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.</p>
+                                </div>
+                            </div>
+
+                            <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto" @click="closeModal">Cancel</button>
+                            </div>
+                        </DialogPanel>
+                    </div>
+                </div>
+            </div>
+        </Dialog>
+    </TransitionRoot>
+</template>
+  
+<script>
+import {
+    Dialog,
+    DialogPanel,
+    DialogTitle,
+    DialogDescription,
+} from '@headlessui/vue'
+
+export default {
+    props: {
+        isOpen: Boolean,
+    },
+    methods: {
+        closeModal() {
+            this.$emit('closemodal');
+        }
+    },
+}
+</script>
