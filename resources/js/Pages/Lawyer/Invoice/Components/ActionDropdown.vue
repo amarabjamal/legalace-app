f29fd0336de972e4260846cd255d96ce105dd63e
Amar Aiman Ab Jamal <amarabjamal@gmail.com>
2023-06-04 16:55:50 +0800

    Moved to Components Folder

--- resources/js/Pages/Lawyer/Invoice/Components/ActionDropdown.vue
+++ resources/js/Pages/Lawyer/Invoice/Components/ActionDropdown.vue
@@ -2,9 +2,7 @@
     <Menu as="div" class="relative inline-block text-left">
     <div>
         <MenuButton class="px-1">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 fill-gray-700">
-            <path fill-rule="evenodd" d="M10.5 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm0 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm0 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" clip-rule="evenodd" />
-            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  class="w-6 h-6 fill-gray-700"><path d="M12 3C10.9 3 10 3.9 10 5C10 6.1 10.9 7 12 7C13.1 7 14 6.1 14 5C14 3.9 13.1 3 12 3ZM12 17C10.9 17 10 17.9 10 19C10 20.1 10.9 21 12 21C13.1 21 14 20.1 14 19C14 17.9 13.1 17 12 17ZM12 10C10.9 10 10 10.9 10 12C10 13.1 10.9 14 12 14C13.1 14 14 13.1 14 12C14 10.9 13.1 10 12 10Z"></path></svg>
         </MenuButton>
     </div>
 
@@ -52,7 +50,7 @@
                         'group flex w-full items-center rounded-md px-3 py-2 text-sm',
                         ]"
                     >
-                        Download PDF
+                        Invoice PDF
                     </a>
                 </MenuItem>
             </div>
@@ -63,11 +61,13 @@
   
 <script>
 import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
-import { EllipsisVertical } from '@heroicons/vue/solid/';
 
 export default {
     components: {
-        Menu, MenuButton, MenuItems, MenuItem, EllipsisVertical
+        Menu, 
+        MenuButton, 
+        MenuItems, 
+        MenuItem,
     },
     props: {
         case_file_id: Number,
