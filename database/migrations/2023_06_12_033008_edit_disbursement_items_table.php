3570bca829afa72303ae24da4311c3c5ac58e460
Amar Aiman Ab Jamal <amarabjamal@gmail.com>
2023-06-13 00:35:51 +0800

    Added migrations

---
+++ database/migrations/2023_06_12_033008_edit_disbursement_items_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('disbursement_items', function (Blueprint $table) {
+            $table->foreignId('claim_voucher_id')->nullable()->after('invoice_id')->constrained('claim_vouchers', 'id');
+        }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
---
+++ database/migrations/2023_06_12_034559_edit_claim_vouchers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('claim_vouchers', function (Blueprint $table) {
+            $table->decimal('amount', 13, 4, true)->nullable()->change();
+        }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
