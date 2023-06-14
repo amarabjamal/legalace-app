9fdcb3dc9b83d4b1187ca632f4d8b71673fd9665
Amar Aiman Ab Jamal <amarabjamal@gmail.com>
2023-06-15 02:59:59 +0800

    Added client broadcaster

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,15 @@ import { InertiaProgress } from '@inertiajs/progress'
 import { Inertia } from '@inertiajs/inertia'
 import Icon from './Shared/Icon';
 import PageHeading from './Shared/PageHeading'
+import Echo from 'laravel-echo';
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+  broadcaster: 'pusher',
+  key: process.env.MIX_PUSHER_APP_KEY,
+  cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+  // Add other configuration options as needed
+});
 
 Inertia.on('success', (event) => {
   let isAuthenticated = event.detail.page.props.auth !== null;
