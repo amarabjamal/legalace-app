84f2278701495a05265f10169b0da7ba313cd156
Amar Aiman Ab Jamal <amarabjamal@gmail.com>
2023-06-15 03:00:32 +0800

    Fixed only show submit for draft

--- resources/js/Pages/Lawyer/ClaimVoucher/Show.vue
+++ resources/js/Pages/Lawyer/ClaimVoucher/Show.vue
@@ -17,7 +17,7 @@
                     </div>
                 </div>
             <div class="flex flex-wrap items-center space-x-2">
-                <form @submit.prevent="submitClaimVoucher">
+                <form v-if="claim_voucher.status === 'Draft'" @submit.prevent="submitClaimVoucher">
                     <button type="submit" class="btn-primary flex items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-4 w-4 inline-block mr-1"><path d="M1.94631 9.31555C1.42377 9.14137 1.41965 8.86034 1.95706 8.6812L21.0433 2.31913C21.5717 2.14297 21.8748 2.43878 21.7268 2.95706L16.2736 22.0433C16.1226 22.5718 15.8179 22.5901 15.5946 22.0877L12.0002 14.0002L18.0002 6.00017L10.0002 12.0002L1.94631 9.31555Z"></path></svg>
                         Submit
@@ -131,7 +131,7 @@ export default {
     },
     data() {
         return {
-            page_title: 'Claim Voucher',
+            page_title: 'Claim Voucher ' + this.claim_voucher.number,
             breadcrumbs: [
                 { link: '/lawyer/dashboard', label: 'Lawyer'},
                 { link: '/lawyer/claim-vouchers/', label: 'Claim Vouchers'},
