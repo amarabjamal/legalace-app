b395ae128166cf2c3832c48477d32b4d13656305
Amar Aiman Ab Jamal <amarabjamal@gmail.com>
2023-06-14 15:36:45 +0800

    Added index and show methods

--- app/Http/Controllers/Admin/ApproveVoucher.php
+++ app/Http/Controllers/Admin/ApproveVoucher.php
@@ -3,17 +3,40 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Inertia\Inertia;
+use App\Models\ClaimVoucher;
 
 class ApproveVoucher extends Controller
 {
     public function index()
     {
 
+        return inertia('Admin/Voucher/Index', [
+            'voucher_requests' => ClaimVoucher::pending()
+                ->where('approver_user_id', '=', auth()->id())
+                ->paginate(25)
+                ->through(fn ($voucher) => [
+                    'id' => $voucher->id,
+                    'ticket_number' => $voucher->ticket_number,
+                    'submission_date' => $voucher->submission_date ? $voucher->submission_date : 'N/A',
+                    'amount' => $voucher->amount->formatTo('en_MY'),
+                    'status' => ClaimVoucher::STATUS[$voucher->status->value],
+                    'requester' => $voucher->requester->name,
+                ]),
+        ]);
+    }
 
-        return Inertia::render('Admin/Voucher/Index', [
+    public function show(ClaimVoucher $voucher_request)
+    {
 
+        return inertia('Admin/Voucher/Show', [
+            'claim_voucher' => $voucher_request->toResource(),
+            'items' => $voucher_request->items->map(fn($item) => 
+                [
+                    'name' => $item->name,
+                    'description' => $item->description,
+                    'amount' => $item->amount->formatTo('en-MY'),
+                ]
+            ),
         ]);
     }
 }
