2ff670c9d00ce383c2142e2d93d6496f8aa9bd33
Amar Aiman Ab Jamal <amarabjamal@gmail.com>
2023-06-16 00:36:50 +0800

    Implemented approveVoucher method

--- app/Http/Controllers/Admin/ApproveVoucher.php
+++ app/Http/Controllers/Admin/ApproveVoucher.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Enums\ClaimVoucherStatusEnum;
+use App\Enums\DisbursementItemStatusEnum;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreClaimVoucherApprovalRequest;
 use App\Models\ClaimVoucher;
+use App\Notifications\ClaimVoucherApprovedNotification;
+use Illuminate\Support\Facades\DB;
 
 class ApproveVoucher extends Controller
 {
@@ -38,5 +43,36 @@ class ApproveVoucher extends Controller
                 ]
             ),
         ]);
+    }
+
+    public function approveVoucher(StoreClaimVoucherApprovalRequest $request, ClaimVoucher $voucher_request)
+    {
+        if($voucher_request->status !== ClaimVoucherStatusEnum::Submitted) {
+            return back()->with('errorMessage', "The claim voucher status is no longer 'Submitted'.");
+        }
+
+        $approvalInputs = $request->all();
+
+        try {
+            DB::transaction(function() use ($voucher_request, $approvalInputs) {
+                $voucher_request->status = ClaimVoucherStatusEnum::Approved;
+                $voucher_request->approval()->create($approvalInputs);
+                $voucher_request->save();
+
+                $voucher_request->items()->update(['status' => DisbursementItemStatusEnum::ApprovedForClaim]);
+                
+                $voucher_request->requester->notify(new ClaimVoucherApprovedNotification($voucher_request, auth()->user()));
+            });
+
+        } catch (\Exception $e) {
+            return back()->with('errorMessage', 'Failed to approve the claim voucher.');           
+        }
+
+        return back()->with('successMessage', 'The claim voucher has been approved.');
+    }
+
+    public function rejectVoucher()
+    {
+        
     }
 }
