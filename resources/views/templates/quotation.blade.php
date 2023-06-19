bc29bd858dd352dc79e7d2d16990bfa63d46869c
Amar Aiman Ab Jamal <amarabjamal@gmail.com>
2023-06-19 22:31:40 +0800

    Updated quotation templates

--- resources/views/templates/quotation.blade.php
+++ resources/views/templates/quotation.blade.php
@@ -3,172 +3,182 @@
 <head>
     <meta charset="UTF-8">
     <title>Quotation</title>
+    <style>
+        {{ file_get_contents(public_path('css/app.css')) }}
 
-    <style type="text/css">
-        * {
-            font-family: Verdana, Arial, sans-serif;
-        }
-        table{
-            font-size: x-small;
-        }
-        tfoot tr td{
-            font-weight: bold;
-            font-size: x-small;
-        }
-
-        .gray {
-            background-color: lightgray
+        body {
+            font-family: Arial, Helvetica, sans-serif;
         }
     </style>
 </head>
 <body>
+    <div class="flex flex-wrap w-full">
+        <div class="flex flex-wrap w-full text-sm">
+            <div class="company-details w-1/2">
+                <img src="{{asset('images/app/logo.png')}}" alt="company logo" class="logo overflow-hidden max-h-16 max-w-16">
+            </div>
+            <div class="receipt-details w-1/2 flex flex-col items-end">
+                <h2 class="mb-4 text-3xl font-semibold uppercase text-gray-900">
+                    Quotation
+                </h2>
+                <dl class="flex flex-col">
+                    <div class="flex text-right">
+                        <dt class="w-28 font-semibold text-gray-700">
+                            Case No. Ref. :
+                        </dt>
+                        <dd class="w-32 text-sm text-gray-600">
+                            {{ $case_file['number'] }}
+                        </dd>
+                    </div>
+                    <div class="flex text-right">
+                        <dt class="w-28 font-semibold text-gray-700">
+                            Date :
+                        </dt>
+                        <dd class="w-32 text-sm text-gray-600">
+                            {{ $date }}
+                        </dd>
+                    </div>
+                </dl>
+            </div>
+        </div>
 
-  <table width="100%">
-    <tr>
-        <td valign="top"><img src="{{ asset('images/app/logo.png')}}" width="150"/></td>
-        <td align="right">
-            <h3>Company Name</h3>
-            <pre>
-                Company representative name
-                Company address
-                Tax ID
-                phone
-                fax
-            </pre>
-        </td>
-    </tr>
+        <div class="flex flex-wrap w-full mt-6 mb-6 pt-4 text-sm">
+            <div class="flex flex-col w-1/2">
+                <div class="font-semibold text-gray-900">
+                    From
+                </div>
+                <div class="flex flex-col mt-3 text-gray-500">
+                    <div class="font-medium text-gray-900 mb-2">
+                        {{$case_file['company']['name'] }}
+                    </div>
+                    <div>
+                        {{$case_file['company']['address'] }}
+                    </div>
+                </div>  
+            </div>
+            <div class="flex flex-col w-1/2">
+                <div class="font-semibold text-gray-900">
+                    To
+                </div>
+                <div class="flex flex-col mt-3 text-gray-500">
+                    <div class="font-medium text-gray-900 mb-2">
+                        {{$case_file['client']['name'] }}
+                    </div>
+                    <div>
+                        {{$case_file['client']['address'] }}
+                    </div>
+                </div>  
+            </div>
+        </div>
 
-  </table>
+        <table class="w-full mt-4 whitespace-nowrap text-left text-sm leading-6">
+            <thead class="bg-gray-900 border border-gray-900 text-gray-100">
+                <tr>
+                    <th class="w-12 px-3 py-2">No.</th>
+                    <th class="px-3 py-2">Item</th>
+                    <th class="w-40 px-3 py-2 text-right">Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($items as $index => $item)
+                <tr>
+                    <td class="max-w-0 px-3 py-2 align-top border border-gray-900">{{ $index + 1 }}</td>
+                    <td class="max-w-0 px-3 py-2 align-top border border-gray-900">
+                        <div class="truncate font-medium text-gray-900">
+                            {{ $item['description'] }}
+                        </div>
+                    </td>
+                    <td class="px-3 py-2 align-top text-right tabular-nums border border-gray-900">
+                        {{ $item['fee'] }}
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th scope="row" colspan="2" class="border border-gray-900 px-3 py-2 text-right font-normal text-gray-700">
+                        Subtotal
+                    </th>
+                    <td colspan="1" class="border border-gray-900 px-3 py-2 text-right text-gray-900 tabular-nums">
+                        {{ $subtotal }}
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row" colspan="2" class="border border-gray-900 px-3 py-2 text-right font-normal text-gray-700">
+                        Tax (0%)
+                    </th>
+                    <td colspan="1" class="border border-gray-900 px-3 py-2 text-right tabular-nums text-gray-900">
+                        {{ $tax }}
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row" colspan="2" class=" border border-gray-900 px-3 py-2 text-right font-semibold text-gray-900">
+                        Total
+                    </th>
+                    <td colspan="1" class="border border-gray-900 px-3 py-2 text-right font-semibold tabular-nums text-gray-900">
+                        {{ $total }}
+                    </td>
+                </tr>
+            </tfoot>
+        </table>
 
-  <table width="100%">
-    <tr>
-        <td>
-            Client Name
-        </td>
-    </tr>
-    <tr>
-        <td>
-            Client Address
-        </td>
-    </tr>
-  </table>
-
-  <table width="100%">
-    <tr>
-        <td>
-            Dear sirs,
-        </td>
-    </tr>
-    <tr>
-        <td>Matter :</td>
-    </tr>
-  </table>
-
-  <table width="100%">
-    <tr>
-        <td>
-        We refer to the above matter.
-        </td>
-    </tr>
-    <tr>
-        <td>
-        Thank you for considering us for the solicitor works in the above matter. We are pleased to provide our fees structure as follows:-
-        </td>
-    </tr>
-  </table>
-
-  <table width="100%">
-    <tr>
-        <td>
-            <h3>1. SCOPE OF SERVICES & LEGAL FEES</h3>
-        </td>
-    </tr>
-  </table>
-
-  <table width="100%">
-    <thead style="background-color: lightgray;">
-      <tr>
-        <th>#</th>
-        <th>Work Description</th>
-        <th>Fee (RM)</th>
-      </tr>
-    </thead>
-    <tbody>
-        @foreach ($workdescriptions as $index => $workdescription)
-            <tr>
-                <th scope="row">{{ $index + 1}}</th>
-                <td>{{ $workdescription['description'] }}</td>
-                <td align="right">{{ $workdescription['fee'] }}</td>
-            </tr>
-        @endforeach
-    </tbody>
-
-    <tfoot>
-        <tr>
-            <td colspan="1"></td>
-            <td align="right">Subtotal</td>
-            <td align="right">0</td>
-        </tr>
-        <tr>
-            <td colspan="1"></td>
-            <td align="right">Tax</td>
-            <td align="right">0</td>
-        </tr>
-        <tr>
-            <td colspan="1"></td>
-            <td align="right">Total</td>
-            <td align="right" class="gray">0</td>
-        </tr>
-    </tfoot>
-  </table>
-
-  <table width="100%">
-    <tr>
-        <td>
+        <div class="mt-4">
             <p>The aboce fees structure is quoted based on our current understanding of the case. Final fees charged are subjected to the actual type and amount of work done.</p>
-        </td>
-    </tr>
-  </table>
+        </div>
+        
+        <div class="mt-6">
+            <h3 class="mb-4 font-bold">2. DISBURSEMENTS</h3>
+            <p class="">The above quotation does not include any disbursements. Disbursements will be charged on incurred basis.</p>
+        </div>
 
-  <table width="100%">
-    <tr>
-        <td>
-            <h3>2. DISBURSEMENTS</h3>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <p>The above quotation does not include any disbursements. Disbursements will be charged on incurred basis.</p>
-        </td>
-    </tr>
-  </table>
+        <div class="mt-6">
+            <h3 class="mb-4 font-bold">3. TAX</h3>
+            <p>As of the date of this letter, no service tax will be imposed on the legal fees.</p>
+        </div>
 
-  <table width="100%">
-    <tr>
-        <td>
-            <h3>3. TAX</h3>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <p>As of the date of this latter, no service tax will be imposed on the legal fees.</p>
-        </td>
-    </tr>
-  </table>
+        <div class="mt-6">
+            <h3 class="mb-4 font-bold">4. INITIAL DEPOSIT</h3>
+            <p>As a policy of our firm, we will request a sum of deposit towards our fees, and disbursements prior to the commencment of work. For this matter, kindly remit a sum of <strong>{{ $deposit_amount }}</strong>  as inital deposit to our clients' accounts as follow:-</p>
 
-  <table width="100%">
-    <tr>
-        <td>
-            <h3>4. INITIAL DEPOSIT</h3>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <p>As a policy of our firm, we will request a sum of deposit towards our fees, and disbursements prior to the commencment of work. For this matter, kindly remit a sum of <strong>RM{{ $deposit_amount }}</strong>  as inital deposit to our clients' accounts as follows:-</p>
-        </td>
-    </tr>
-  </table>
+            <div class="mt-6 w-9/12">
+                <div>
+                    Account Name: {{ $bank_account['account_name'] }}
+                </div>
+                <div class="mt-2">
+                    Bank Name: {{ $bank_account['bank_name'] }}
+                </div>
+                <div class="mt-2">
+                    Account Number: {{ $bank_account['account_number'] }}
+                </div>
+                <div class="mt-2">
+                    Bank Address: {{ $bank_account['bank_address'] }}
+                </div>
+                <div class="mt-2">
+                    SWIFT Code: {{ $bank_account['swift_code'] }}
+                </div>
+            </div>
+        </div>
 
+        <div class="w-full mt-10">
+            <div class="w-full mb-14 flex justify-center">
+                <h3 class="uppercase font-bold text-lg text-gray-800">Acceptance</h3>
+            </div>
 
+            <div class="mb-10">
+                I, <div class="w-56 inline-block border-b border-gray-600" ></div> (NRIC No.: <div class="w-36 inline-block border-b border-gray-600" ></div>​) hereby confirm that I have engaged your firm to act for me in this matter for the above services. I have read and agreed to the above terms and conditions and I undertake to pay the legal fees and disbursements incurred from time to time upon request.
+            </div>
+
+            <div class="">
+                <div class="w-56 border-b border-gray-600 mb-2"></div>
+                <div class="font-semibold">Name: </div>
+                <div class="font-semibold">Date: </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- <div class="fixed bottom-0 left-0 w-full text-gray-500 text-xs p-2 border-t">
+        This invoice is generated electronically and does not require a physical signature.
+        <div class="text-center mt-4 font-bold">Generated by Legal Ace &copy; 2023</div>
+    </div> --}}
 </body>
 </html>
