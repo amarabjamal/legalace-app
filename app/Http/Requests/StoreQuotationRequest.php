be95e10bb357a5dec3f3d49f0e6a45439928b8c8
Amar Aiman Ab Jamal <amarabjamal@gmail.com>
2023-06-24 17:33:24 +0800

    Added case file id to check is conflict resolved

--- app/Http/Requests/StoreQuotationRequest.php
+++ app/Http/Requests/StoreQuotationRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class StoreQuotationRequest extends FormRequest
 {
@@ -27,6 +28,7 @@ class StoreQuotationRequest extends FormRequest
         $this->merge([
             'company_id' => auth()->user()->company_id,
             'created_by_user_id' => auth()->id(),
+            'case_file_id' => $this->case_file->id,
         ]);
     }
 
@@ -38,6 +40,9 @@ class StoreQuotationRequest extends FormRequest
     public function rules()
     {
         return [
+            'case_file_id' => ['required', Rule::exists('case_files', 'id')->where(function ($query) {
+                $query->where('no_conflict_checked', true);
+            })],
             'work_descriptions' => ['required', 'array'],
             'work_descriptions.*.description' => ['required', 'string'],
             'work_descriptions.*.fee' => ['required', 'numeric', 'decimal:2', 'between:0.01,999999999.99'],
@@ -49,6 +54,7 @@ class StoreQuotationRequest extends FormRequest
     public function messages()
     {
         return [
+            'case_file_id.exists' => 'The case file must be declared as no conflict before proceed.',
             'work_descriptions.required' => 'The item list cannot be empty.',
             'work_descriptions.*.description.required' => 'The work desc :position cannot be empty.',
             'work_descriptions.*.fee.between' => 'The work fee :position must be between 0.01 and 999999999.99.',
