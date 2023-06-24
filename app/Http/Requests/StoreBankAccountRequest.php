8aba071d288eb8d45cade3ac1f9f65e65d036285
Amar Aiman Ab Jamal <amarabjamal@gmail.com>
2023-06-24 17:32:30 +0800

    Refactored

--- app/Http/Requests/StoreBankAccountRequest.php
+++ app/Http/Requests/StoreBankAccountRequest.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 
 class StoreBankAccountRequest extends FormRequest
@@ -19,6 +18,19 @@ class StoreBankAccountRequest extends FormRequest
     }
 
     /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'created_by_user_id' => auth()->id(),
+            'company_id' => auth()->user()->company_id,
+        ]);
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, mixed>
@@ -26,47 +38,18 @@ class StoreBankAccountRequest extends FormRequest
     public function rules()
     {
         return [
-            'label' => [
-                'required', 
-                'string', 
-                Rule::unique('bank_accounts')->where(fn ($query) => $query->where('company_id', Auth::user()->company_id)),
+            'label' => ['required', 'string', 
+                Rule::unique('bank_accounts')->where(fn ($query) => $query->where('company_id', auth()->user()->company_id)),
             ],
-            'account_name' => [
-                'required', 
-                'string'
+            'bank_account_type_id' => ['required', 'exists:bank_account_types,id'],
+            'account_name' => ['required', 'string'],
+            'account_number' => ['required', 'numeric', 'digits_between:6,17',
+                Rule::unique('bank_accounts')->where(fn ($query) => $query->where('company_id', auth()->user()->company_id)),
             ],
-            'bank_name' => [
-                'required', 
-                'string'
-            ],
-            'account_number' => [
-                'required', 
-                Rule::unique('bank_accounts')->where(fn ($query) => $query->where('company_id', Auth::user()->company_id)),
-                'numeric', 
-                'digits_between:6,17'
-            ],
-            'bank_address' => [
-                'required', 
-                'string'
-            ],
-            'swift_code' => [
-                'required', 
-                'string', 
-                'regex:/^([a-zA-Z]){4}([a-zA-Z]){2}([0-9a-zA-Z]){2}([0-9a-zA-Z]{3})?$/'
-            ],
-            'bank_account_type_id' => [
-                'required', 
-                'exists:bank_account_types,id'
-            ],
+            'swift_code' => ['required', 'string', 'regex:/^([a-zA-Z]){4}([a-zA-Z]){2}([0-9a-zA-Z]){2}([0-9a-zA-Z]{3})?$/'],
+            'opening_balance' => ['required', 'numeric', 'decimal:2', 'between:0,999999999.99'],
+            'bank_name' => ['required', 'string'],
+            'bank_address' => ['required', 'string'],
         ];
-    }
-
-
-    public function validated($key = null, $default = null)
-    {
-        return array_merge(parent::validated(), [
-            'created_by' => Auth::id(),
-            'company_id' => Auth::user()->company_id,
-        ]);
     }
 }
