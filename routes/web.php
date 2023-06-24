9606c672a4ed42019634840b3a972d6254af55fa
Amar Aiman Ab Jamal <amarabjamal@gmail.com>
2023-06-24 17:38:44 +0800

    Added routes and applied quotation.paid middleware

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\Lawyer\ClientAccountController;
 use App\Http\Controllers\Lawyer\AccountReportingController;
 use App\Http\Controllers\Lawyer\CaseFileController;
 use App\Http\Controllers\Lawyer\ClaimVoucherController;
+use App\Http\Controllers\Lawyer\ConflictReportController;
 use App\Http\Controllers\Lawyer\DisbursementItemController;
 use App\Http\Controllers\Lawyer\InvoiceController;
 use App\Http\Controllers\Lawyer\InvoicePaymentController;
@@ -72,7 +73,7 @@ Route::group(['middleware' => 'auth'], function() {
         Route::resource('users', UserController::class)->except('show');
 
         // Bank Accounts
-        Route::resource('bank-accounts', BankAccountController::class)->except('show');
+        Route::resource('bank-accounts', BankAccountController::class);
     
         // Company Profile
         Route::singleton('company', CompanyController::class)->except('destroy');
@@ -94,35 +95,39 @@ Route::group(['middleware' => 'auth'], function() {
         Route::resource('claim-vouchers', ClaimVoucherController::class);
 
         Route::scopeBindings()->prefix('/case-files/{case_file}')->group(function() {
+            Route::post('/resolve-no-conflict', [CaseFileController::class, 'resolveNoConflict']);
+
+            // Conflict Reports
+            Route::resource('/conflict-reports', ConflictReportController::class)->only(['index', 'store']);
+
             // Quotation/Estimate
             Route::prefix('/quotation')->group(function() {
                 Route::get('/pdf', [QuotationController::class, 'downloadPdf']);
-                Route::get('/email', [QuotationController::class, 'emailQuotation']);
+                Route::post('/email-quotation', [QuotationController::class, 'emailQuotation']);
+                Route::post('/mark-sent', [QuotationController::class, 'markSent']);
             });
             Route::singleton('quotation', QuotationController::class)->creatable();
 
-            // Invoices
-            Route::prefix('/invoices/{invoice}')->group(function() {
-                Route::post('set-open', [InvoiceController::class, 'setOpen']);
-                Route::post('email-invoice', [InvoiceController::class, 'emailInvoice']);
-                Route::get('pdf', [InvoiceController::class, 'downloadPdf']);
-
-                // Payment
-                Route::get('/payment/receipt', [InvoicePaymentController::class, 'downloadReceipt']);
-                Route::singleton('payment', InvoicePaymentController::class)->creatable()->only('create', 'store');
-
-                // Receipt
-                Route::get('receipt/pdf', [ReceiptController::class, 'downloadPdf']);
-                Route::post('receipt/mark-sent', [ReceiptController::class, 'markSent']);
-                Route::post('receipt/email-receipt', [ReceiptController::class, 'emailReceipt']);
-                Route::singleton('receipt', ReceiptController::class)->creatable();
-
+            Route::group(['middleware' => 'quotation.paid'], function() {
+                Route::group(['prefix' => '/invoices/{invoice}'], function() {
+                    Route::post('set-open', [InvoiceController::class, 'setOpen']);
+                    Route::post('email-invoice', [InvoiceController::class, 'emailInvoice']);
+                    Route::get('pdf', [InvoiceController::class, 'downloadPdf']);
+    
+                    // Payment
+                    Route::get('/payment/receipt', [InvoicePaymentController::class, 'downloadReceipt']);
+                    Route::singleton('payment', InvoicePaymentController::class)->creatable()->only('create', 'store');
+    
+                    // Receipt
+                    Route::get('receipt/pdf', [ReceiptController::class, 'downloadPdf']);
+                    Route::post('receipt/mark-sent', [ReceiptController::class, 'markSent']);
+                    Route::post('receipt/email-receipt', [ReceiptController::class, 'emailReceipt']);
+                    Route::singleton('receipt', ReceiptController::class)->creatable();
+                });
+    
+                Route::resource('disbursement-items', DisbursementItemController::class);
+                Route::resource('invoices', InvoiceController::class);
             });
-
-            Route::resource('disbursement-items', DisbursementItemController::class);
-            Route::resource('invoices', InvoiceController::class);
         });
-        
-        Route::get('/getbankaccountdetails/{bank_account}', [BankAccountController::class, 'getBankAccountDetails']);
     });
 });
