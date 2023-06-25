4125a8037e3469d7ebdbec78132e1d66b1f9f88b
Amar Aiman Ab Jamal <amarabjamal@gmail.com>
2023-06-26 05:20:38 +0800

    Added add payment function

---
+++ app/Http/Requests/StoreQuotationPaymentRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\PaymentMethodEnum;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+
+class StoreQuotationPaymentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'company_id' => auth()->user()->company_id,
+            'created_by_user_id' => auth()->id(),
+            'amount' => $this->case_file->quotation->total,
+        ]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'date' => ['required', 'date', 'before_or_equal:today'],
+            'client_bank_account_id' => ['required', 'exists:bank_accounts,id'],
+            'payment_method_code' => ['required', new Enum(PaymentMethodEnum::class)],
+            'receipt' => ['required', 'file', 'mimes:pdf,xlxs,xlx,docx,doc,csv,txt,png,gif,jpg,jpeg', 'max:2048'],
+            'description' => ['nullable', 'string', 'max:255'],
+        ];
+    }
+}
