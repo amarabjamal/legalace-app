c0d7e198ce4e7d1809c73c4290fbd755865e7125
Amar Aiman Ab Jamal <amarabjamal@gmail.com>
2024-01-29 10:26:29 +0800

    Cleaned up

--- resources/js/Pages/Lawyer/Invoice/Show.vue
+++ resources/js/Pages/Lawyer/Invoice/Show.vue
@@ -248,61 +248,17 @@
                 </div>
             </div>
         </div>
-        
-        <!-- <div v-if="invoice.status_value === 4"  class="w-full max-w-3xl h-fit xl:max-w-xs bg-white border border-gray-300 rounded-md overflow-hidden">
-            <dl class="flex flex-wrap">
-                <div class="flex-auto pl-6 pt-6">
-                    <dt class="text-sm font-semibold leading-6 text-gray-900">Amount</dt>
-                    <dd class="mt-1 text-base font-semibold leading-6 text-gray-900">{{ invoice.payment.amount }}</dd>
-                </div>
-                <div class="mt-6 flex w-full flex-none gap-x-4 border-t border-gray-900/[.05] px-6 pt-6">
-                    <dt class="flex-none">
-                        <span class="sr-only">Payment date</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="h-6 w-5 text-gray-400"><path d="M5.25 12a.75.75 0 01.75-.75h.01a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75H6a.75.75 0 01-.75-.75V12zM6 13.25a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75h.01a.75.75 0 00.75-.75V14a.75.75 0 00-.75-.75H6zM7.25 12a.75.75 0 01.75-.75h.01a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75H8a.75.75 0 01-.75-.75V12zM8 13.25a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75h.01a.75.75 0 00.75-.75V14a.75.75 0 00-.75-.75H8zM9.25 10a.75.75 0 01.75-.75h.01a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75H10a.75.75 0 01-.75-.75V10zM10 11.25a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75h.01a.75.75 0 00.75-.75V12a.75.75 0 00-.75-.75H10zM9.25 14a.75.75 0 01.75-.75h.01a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75H10a.75.75 0 01-.75-.75V14zM12 9.25a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75h.01a.75.75 0 00.75-.75V10a.75.75 0 00-.75-.75H12zM11.25 12a.75.75 0 01.75-.75h.01a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75H12a.75.75 0 01-.75-.75V12zM12 13.25a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75h.01a.75.75 0 00.75-.75V14a.75.75 0 00-.75-.75H12zM13.25 10a.75.75 0 01.75-.75h.01a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75H14a.75.75 0 01-.75-.75V10zM14 11.25a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75h.01a.75.75 0 00.75-.75V12a.75.75 0 00-.75-.75H14z"></path><path fill-rule="evenodd" d="M5.75 2a.75.75 0 01.75.75V4h7V2.75a.75.75 0 011.5 0V4h.25A2.75 2.75 0 0118 6.75v8.5A2.75 2.75 0 0115.25 18H4.75A2.75 2.75 0 012 15.25v-8.5A2.75 2.75 0 014.75 4H5V2.75A.75.75 0 015.75 2zm-1 5.5c-.69 0-1.25.56-1.25 1.25v6.5c0 .69.56 1.25 1.25 1.25h10.5c.69 0 1.25-.56 1.25-1.25v-6.5c0-.69-.56-1.25-1.25-1.25H4.75z" clip-rule="evenodd"></path></svg>
-                    </dt>
-                    <dd class="text-sm leading-6 text-gray-500">
-                        Paid on <time class="font-medium text-gray-900">{{ invoice.payment.date }}</time>
-                    </dd>
-                </div>
-                <div class="mt-4 flex w-full flex-none gap-x-4 px-6">
-                    <dt class="flex-none">
-                        <span class="sr-only">Payment method</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="h-6 w-5 text-gray-400"><path fill-rule="evenodd" d="M2.5 4A1.5 1.5 0 001 5.5V6h18v-.5A1.5 1.5 0 0017.5 4h-15zM19 8.5H1v6A1.5 1.5 0 002.5 16h15a1.5 1.5 0 001.5-1.5v-6zM3 13.25a.75.75 0 01.75-.75h1.5a.75.75 0 010 1.5h-1.5a.75.75 0 01-.75-.75zm4.75-.75a.75.75 0 000 1.5h3.5a.75.75 0 000-1.5h-3.5z" clip-rule="evenodd"></path></svg>
-                    </dt>
-                    <dd class="text-sm leading-6 text-gray-500">Through <span class="font-medium text-gray-900">{{ invoice.payment.method }}</span></dd>
-                </div>
-                <div class="mt-4 flex w-full flex-none gap-x-4 px-6">
-                    <dt class="flex-none">
-                        <span class="sr-only">Recorded by</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="h-6 w-5 text-gray-400"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z" clip-rule="evenodd"></path></svg>
-                    </dt>
-                    <dd class="text-sm leading-6 text-gray-500">Recorded by <span class="font-medium text-gray-900">{{ invoice.payment.created_by.name }}</span></dd>
-                </div>
-            </dl>
-
-            <div class="mt-6 border-t border-gray-900/[.05] p-6">
-                <a 
-                    :href="`/lawyer/case-files/${case_file.id}/invoices/${invoice.id}/payment/receipt`" 
-                    target="_blank"
-                    class="text-sm font-semibold leading-6 text-gray-900"
-                >
-                    Download receipt <span aria-hidden="true">→</span>
-                </a>
-            </div>
-        </div> -->
     </div>
 </template>
 
 <script>
 import Layout from '../Shared/Layout';
-import ActionDropdown from './Components/ActionDropdown';
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
 import { ChevronUpIcon } from "@heroicons/vue/outline"; 
 import { cva } from 'class-variance-authority';
 
 export default { 
     components: { 
-        ActionDropdown,
         Disclosure,
         DisclosureButton,
         DisclosurePanel,
